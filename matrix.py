a561f10466d81ed1641cc6237b9cf2e76bd26103
Wesley Clark <iamwesleyclark@gmail.com>
2021-06-16 22:20:08 -0400

    Add files via upload

---
+++ matrix.py
@@ -0,0 +1,91 @@
+"""Matrix:
+
+Special case of two dimensional array where each data element is of strictly the same size.
+
+These are important for some mathematical and scientific calculations."""
+
+# use numpy for this one
+
+from numpy import *
+
+a = array([['Mon',18,20,22,17],['Tue',11,18,21,18],
+		   ['Wed',15,21,20,19],['Thu',11,20,22,21],
+		   ['Fri',18,17,23,22],['Sat',12,22,20,18],
+		   ['Sun',13,15,19,16]])
+    
+m = reshape(a,(7,5))
+print(m)
+
+# accessing data can be done using indexes, same as a 2d array
+
+from numpy import *
+
+a = array([['Mon',18,20,22,17],['Tue',11,18,21,18],
+		   ['Wed',15,21,20,19],['Thu',11,20,22,21],
+		   ['Fri',18,17,23,22],['Sat',12,22,20,18],
+		   ['Sun',13,15,19,16]])
+    
+m = reshape(a,(7,5))
+print(m[2])
+
+
+# add a row
+
+from numpy import *
+a = array([['Mon',18,20,22,17],['Tue',11,18,21,18],
+		   ['Wed',15,21,20,19],['Thu',11,20,22,21],
+		   ['Fri',18,17,23,22],['Sat',12,22,20,18],
+		   ['Sun',13,15,19,16]])
+
+m_r = append(m, [['Av', 12, 15, 13, 11]], 0)
+
+print(m_r)
+
+# add a column:
+
+from numpy import * 
+m = array([['Mon',18,20,22,17],['Tue',11,18,21,18],
+		   ['Wed',15,21,20,19],['Thu',11,20,22,21],
+		   ['Fri',18,17,23,22],['Sat',12,22,20,18],
+		   ['Sun',13,15,19,16]])
+
+m_c = insert(m,[5],[[1],[2],[3],[4],[5],[6],[7]],1)
+
+print(m_c)
+
+
+# delete a row from a matrix using delete() method.  Specify the index and row:
+
+from numpy import * 
+m = array([['Mon',18,20,22,17],['Tue',11,18,21,18],
+		   ['Wed',15,21,20,19],['Thu',11,20,22,21],
+		   ['Fri',18,17,23,22],['Sat',12,22,20,18],
+		   ['Sun',13,15,19,16]])
+    
+m = delete(m,[2],0)
+
+
+# delete a column from a matrix
+
+from numpy import * 
+m = array([['Mon',18,20,22,17],['Tue',11,18,21,18],
+		   ['Wed',15,21,20,19],['Thu',11,20,22,21],
+		   ['Fri',18,17,23,22],['Sat',12,22,20,18],
+		   ['Sun',13,15,19,16]])
+
+m = delete(m,s_[2],1)
+
+print(m)
+
+
+# update a row in a matrix
+
+from numpy import * 
+m = array([['Mon',18,20,22,17],['Tue',11,18,21,18],
+		   ['Wed',15,21,20,19],['Thu',11,20,22,21],
+		   ['Fri',18,17,23,22],['Sat',12,22,20,18],
+		   ['Sun',13,15,19,16]])
+
+m[3] = ['Thu', 0,0,0,0]    
+print(m)
+
